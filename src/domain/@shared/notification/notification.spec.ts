33646aa01b11047d852f84dc1e621e009ff493e8
Rafiere <rafapgbr@gmail.com>
2023-08-19 11:37:51 -0300

    feat: add notification and its tests

---
+++ src/domain/@shared/notification/notification.spec.ts
@@ -0,0 +1,68 @@
+import {Not} from "sequelize-typescript/dist/browser";
+import Notification from "./notification";
+
+describe("unit tests for notifications", () => {
+
+    it("should create errors", () => {
+        const notification = new Notification();
+
+        /* Se recebermos a mensagem "ID is required", o "context" mostra se o ID é de
+        * "customer" ou de "order", por exemplo. */
+        const error = {
+            message: "error message",
+            context: "customer"
+        }
+
+        notification.addError(error);
+
+        expect(notification.messages("customer")).toBe("customer: error message,")
+
+        const error2 = {
+            message: "error message 2",
+            context: "customer"
+        }
+
+        notification.addError(error2);
+
+        expect(notification.messages("customer")).toBe("customer: error message,customer: error message 2,");
+
+        const error3 = {
+            message: "error message 3",
+            context: "order"
+        }
+
+        notification.addError(error3);
+
+        expect(notification.messages("customer")).toBe("customer: error message,customer: error message 2,");
+
+        expect(notification.messages()).toBe("customer: error message,customer: error message 2,order: error message 3,");
+    })
+
+    it("should check if notification has at least one error", () => {
+
+        const notification = new Notification();
+
+        const error = {
+            message: "error message",
+            context: "customer"
+        }
+
+        notification.addError(error);
+
+        expect(notification.hasErrors()).toBe(true);
+    })
+
+
+    it("should get all errors props", () => {
+        const notification = new Notification();
+
+        const error = {
+            message: "error message",
+            context: "customer"
+        }
+
+        notification.addError(error);
+
+        expect(notification.getErrors()).toEqual([error]);
+    })
+})
