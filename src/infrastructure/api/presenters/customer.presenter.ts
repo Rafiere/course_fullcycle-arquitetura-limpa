c0b717b04afc14fb895e479926fc5f3a6a259625
Rafiere <rafapgbr@gmail.com>
2023-08-19 11:38:20 -0300

    feat: add presenter for customer

---
+++ src/infrastructure/api/presenters/customer.presenter.ts
@@ -0,0 +1,30 @@
+import {toXML} from 'jstoxml';
+import {OutputListCustomerDto} from "../../../usecase/customer/list/list.customer.dto";
+
+export default class CustomerPresenter {
+
+    static toXML(data: OutputListCustomerDto): string {
+
+        const xmlOption = {
+            header: true,
+            indent: '  ',
+            newLine: "\n",
+            allowEmpty: true
+        }
+
+        return toXML({
+            customers: {
+                customer: data.customers.map((customer) => ({
+                    id: customer.id,
+                    name: customer.name,
+                    address: {
+                        street: customer.address.street,
+                        city: customer.address.city,
+                        number: customer.address.number,
+                        zip: customer.address.zip
+                    }
+                }))
+            }
+        }, xmlOption);
+    }
+}
